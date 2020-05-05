5e3f7b68b7702e0db6abab266caeff4867b1775b
Desta F Muchamad <desta.08b@gmail.com>
2020-05-05 07:04:15 +0700

    Fetching Image .. Next Styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
-import Counter from "./components/Counter";
 
-function App() {
-  return (
-    <div className="App">
-          <h1>Hello React ParcelJS</h1>
-          <Counter/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    images: [],
+  };
+
+  componentDidMount() {
+    fetch("https://picsum.photos/list")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          images: data.slice(0, 30),
+        });
+      });
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <h1> 📸 Picsum </h1>
+        {this.state.images.map((image) => (
+          <img src={`https://picsum.photos/200/300?image=${image.id}`} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
