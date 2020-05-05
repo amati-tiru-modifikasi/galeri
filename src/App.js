4da7cad2062b868dc6587c7441c9cc1f66bd89f0
Desta F Muchamad <desta.08b@gmail.com>
2020-05-06 04:34:01 +0700

    Sylted Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+body {
+  margin: 0;
+}
+
+h1 {
+  margin: 0;
+}
+`;
+
+const HeaderWrap = styled.h1`
+  padding: 16vh;
+  background-color: hsl(0 0% 94%);
+`;
+
+const TileView = styled.div`
+  display: grid;
+  padding: 16vh;
+  grid-gap: 16vh;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+`;
+
+const Photo = styled.img`
+  display: block;
+  width: 100%;
+  object-fit: cover;
+`;
 
 class App extends React.Component {
   state = {
@@ -17,12 +46,15 @@ class App extends React.Component {
   render() {
     console.log(this.state);
     return (
-      <div>
-        <h1> 📸 Picsum </h1>
-        {this.state.images.map((image) => (
-          <img src={`https://picsum.photos/200/300?image=${image.id}`} />
-        ))}
-      </div>
+      <React.Fragment>
+        <GlobalStyles />
+        <HeaderWrap> 📸 Picsum </HeaderWrap>
+        <TileView>
+          {this.state.images.map((image) => (
+            <Photo src={`https://picsum.photos/200/300?image=${image.id}`} />
+          ))}
+        </TileView>
+      </React.Fragment>
     );
   }
 }
